e152cb3e7cefc7e5c73e9c02bc623ad47dd4c9ae
Lenore8963 <zhouchen8963@gmail.com>
2024-07-07 19:03:18 -0700

    display correct tuits on home screen

--- src/home-screen/index.js
+++ src/home-screen/index.js
@@ -1,16 +1,26 @@
-import React from "react";
+import React, { useContext } from "react";
 import Title from "./title";
 import PostInput from "./posts";
 import TuitList from "../tuits";
 import DogOfTheDay from "./dog-of-the-day";
 import Welcome from "./welcome";
+import { AuthContext } from "../auth-context";
 
 const HomeScreen = ({ addTuit, tuits, deleteTuit }) => {
+  const { user } = useContext(AuthContext);
+
+  // Filter tuits to display only those from the current user and followed users
+  const filteredTuits = tuits.filter(
+    (tuit) =>
+      tuit.userId._id === user._id ||
+      user.followedUsers.includes(tuit.userId._id)
+  );
+
   return (
     <div>
       <Title />
       <PostInput addTuit={addTuit} />
-      <TuitList tuits={tuits} handleDelete={deleteTuit} />
+      <TuitList tuits={filteredTuits} handleDelete={deleteTuit} />
       <Welcome />
       <DogOfTheDay />
     </div>
